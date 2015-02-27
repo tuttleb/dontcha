3d6118d34e3eccbd22150d4ef50c602d2c98a9b7
Brett Tuttle <tuttle.brett@gmail.com>
2015-02-27 17:56:04 -0500

    Began generation function for random unicode strings

--- dontcha.py
+++ dontcha.py
@@ -1,5 +1,6 @@
 import PIL
 from PIL import Image, ImageFont, ImageDraw
+import random
 
 def main():
     dontcha = Dontcha()
@@ -14,9 +15,23 @@ def main():
 class Dontcha():
     def __init__(self):
         self.font = PIL.ImageFont.truetype(filename='Pixel-UniCode.ttf')
+        self._buildUnicodeCharList()
 
-    def _getText(self, text):
-        return self.font.getmask(text)
+    def generateWord(self):
+        length = random.randint(10,20)
+
+        word = ""
+
+        for i in range(length):
+            c = chr(random.choice(self.chars))
+            word += c
+
+        return word
+
+    def _buildUnicodeCharList(self):
+        self.chars = list(range(0x21, 0x7E)) + \
+                list(range(0xA1, 0xAC))
+
 
 if __name__ == "__main__":
     main()
